8fe9339e550f07aecbe237d7b57286fa1cd55dab
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-10 21:59:09 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,4 +1,14 @@
 def unsafe?(speed)
+  
+  if speed > 60
+    true
+  
+  elsif speed < 40
+    true
+  
+  else
+    false
+end
 
 end
 
@@ -6,6 +16,8 @@ end
 
 def not_safe?(speed)
 	
+	speed > 60 || speed < 40 ? true : false
+	  
 end
 	
 
